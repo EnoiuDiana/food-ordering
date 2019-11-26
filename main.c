dbddf07acf92dd1186f39ecca88b5d6e2f74e747
EnoiuDiana <edianagte@yahoo.com>
2019-11-26 22:01:12 +0200

    loading data

--- main.c
+++ main.c
@@ -1,22 +1,72 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "userdata.h"
 #include "display.h"
 #include "choice.h"
 
 int main() {
+    char string[300],line[10];
+    char *pt;
+    int nrOfFoodTypes,nrDrinks,nrSpecType[MAX_NR_SPEC_TYPES],k;
+    printf("PLease load the data:\n");
+    gets(line);
+    sscanf(line,"%d",&nrOfFoodTypes);
+    char ** foodTypes = (char**)malloc(nrOfFoodTypes * sizeof(char*));
+    char *** specFoods = (char***)malloc(nrOfFoodTypes * sizeof(char**));
+    double ** priceFoods = (double**)malloc(nrOfFoodTypes * sizeof(double*));
+    for(int i=0;i<nrOfFoodTypes;i++) {
+        foodTypes[i] = (char*)malloc(MAX_FOOD_TYPE_NAME * sizeof(char));
+        specFoods[i] = (char**)malloc(MAX_NR_SPEC_TYPES * sizeof(char*));
+        priceFoods[i] = (double*)malloc(MAX_NR_SPEC_TYPES * sizeof(double));
+        gets(string);
+        pt = strtok(string, ":");
+        strcpy(foodTypes[i],pt);
+
+        k = 0;//if k even it extracts food name, if k odd it extracts price
+        while (pt != NULL) {
+            if (k % 2 == 0) {
+                pt = strtok(NULL, "(");
+                if(pt==NULL){continue;}//if i dont use this it will crash
+                pt = strtok(NULL, "-");
+                specFoods[i][k/2] = (char*)malloc(MAX_SPEC_TYPE_NAME * sizeof(char));
+                strcpy(specFoods[i][k/2],pt);
+                specFoods[i][k/2][strlen(pt)-1]='\0';
+                nrSpecType[i]=k/2;
+            } else {
+                pt = strtok(NULL, ")");
+                sscanf(pt, "%lf", &priceFoods[i][k / 2]);
+            }
+            k++;
+        }
+    }
+    gets(line);
+    sscanf(line,"%d",&nrDrinks);
+    char ** drinks = (char**)malloc(nrDrinks * sizeof(char*));
+    double * pricesDrinks = (double*)malloc(nrDrinks * sizeof(double));
+    gets(string);
+    strrev(string); // reversed the string so that if drinks name contain '-' it won't affect the program
+    pt = strtok(string,"-");
+    for(int i=0;i<nrDrinks;i++){
+        sscanf(strrev(pt),"%lf",&pricesDrinks[i]);
+        drinks[i] = (char*)malloc(MAX_DRINK_NAME * sizeof(char));
+        pt = strtok(NULL,"(");
+        strcpy(drinks[i],pt);
+        strrev(drinks[i]);
+        if(pt==NULL){continue;}
+        pt = strtok(NULL,"-");
+    }
+    for(int i=0;i<nrOfFoodTypes;i++){
+        printf("%s: ", foodTypes[i]);
+        for(int j=0;j<=nrSpecType[i];j++){
+            printf("(%s - %.2lf) ", specFoods[i][j], priceFoods[i][j]);
+        }
+        printf("\n");
+    }
     printf("Welcome to Food Thingies!\nPlease sign in to continue!\n");
     // food data
-    int nrOfFoodTypes = 3,nrSpecType[] = {3,2,4},nrDrinks = 4, pricesDrinks[] = {5,5,5,4}, prices[3][MAX_NR_PRICES] = {
-            {21,23,19},
-            {23,21},
-            {23,22,19,21}
-    };
-    char foodTypes[][MAX_FOOD_TYPE_NAME] = {"Pizza","Pasta","Salad"}, specTypes[3][MAX_NR_SPEC_TYPES][MAX_SPEC_TYPE_NAME] = {
-            {"Pizza Carbonara","Pizza Diavola","Pizza Margherita"},
-            {"Chicken alfredo","Mac&cheese"},
-            {"Tuna Salad","Chicken Salad","Greek Salad","Cobb"}
-    };
-    char drinks[][MAX_DR_CUT_NAME] = {"Coca-Cola","Fanta","Lipton","Water"},cutlery[][MAX_DR_CUT_NAME] = {"Yes","No,thanks"}, addInfo[200];
+
+    char cutlery[][MAX_CUTLERY_NAME] = {"Yes","No,thanks"}, addInfo[200];
     //user input
     char username[20], password[20];
     int typeChoice, specTypeChoice, drinkChoice, cutleryChoice, confirmChoice;
@@ -35,7 +85,7 @@ int main() {
                 break;
             }
             case 2: {
-                displaySpecificFoods(nrSpecType[typeChoice],foodTypes[typeChoice],specTypes[typeChoice],prices[typeChoice]);
+                displaySpecificFoods(nrSpecType[typeChoice], foodTypes[typeChoice], specFoods[typeChoice], priceFoods[typeChoice]);
                 specTypeChoice = getChoiceIndex(nrSpecType[typeChoice], &state);
                 break;
             }
@@ -54,8 +104,8 @@ int main() {
                 break;
             }
             case 6: {
-                displayOrder(specTypes[typeChoice][specTypeChoice],prices[typeChoice][specTypeChoice],
-                        drinks[drinkChoice],pricesDrinks[drinkChoice],cutlery[cutleryChoice],addInfo,username);
+                displayOrder(specFoods[typeChoice][specTypeChoice], priceFoods[typeChoice][specTypeChoice],
+                             drinks[drinkChoice], pricesDrinks[drinkChoice], cutlery[cutleryChoice], addInfo, username);
                 confirmChoice = getChoiceIndex(1, &state);
                 confirmOrder(confirmChoice, &order, &state);
                 break;
