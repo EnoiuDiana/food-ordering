32696a922f8d84d3f553875169c91a66ac71fcf7
EnoiuDiana <edianagte@yahoo.com>
2019-12-04 00:10:26 +0200

    simplifying console reading

--- main.c
+++ main.c
@@ -16,100 +16,56 @@ int main() {
     double **priceFoods;
     char **drinks;
     double *pricesDrinks;
-    if(data == NULL) {
-        char string[MAX_STRING], line[MAX_LINE];
-        char *pt;
+    if(data == NULL){
+        data = stdin;
         printf("PLease load the data:\n");
-        //loading food data
-        gets(line);
-        sscanf(line, "%d", &nrOfFoodTypes);
-        foodTypes = (char **) malloc(nrOfFoodTypes * sizeof(char *));
-        specFoods = (char ***) malloc(nrOfFoodTypes * sizeof(char **));
-        priceFoods = (double **) malloc(nrOfFoodTypes * sizeof(double *));
-        nrSpecType = (int *) malloc(nrOfFoodTypes * sizeof(int));
-        for (int i = 0; i < nrOfFoodTypes; i++) {
-            gets(string);
-            pt = strtok(string, " ");
-            foodTypes[i] = (char *) malloc(MAX_FOOD_TYPE_NAME * sizeof(char));
-            strcpy(foodTypes[i], pt);
-            pt = strtok(NULL, ":");
-            sscanf(pt,"%d",&nrSpecType[i]);
-            specFoods[i] = (char **) malloc(nrSpecType[i] * sizeof(char *));
-            priceFoods[i] = (double *) malloc(nrSpecType[i] * sizeof(double));
-            for(int j=0;j<nrSpecType[i];j++) {
-                pt = strtok(NULL, "-");
-                specFoods[i][j] = (char *) malloc(MAX_SPEC_TYPE_NAME * sizeof(char));
-                strcpy(specFoods[i][j], pt);
-                specFoods[i][j][strlen(pt) - 1] = '\0';
-                strcpy(specFoods[i][j],specFoods[i][j]+2);
-                pt = strtok(NULL, ")");
-                sscanf(pt, "%lf", &priceFoods[i][j]);
-            }
-        }
-        //loading drinks data
-        gets(line);
-        sscanf(line, "%d", &nrDrinks);
-        drinks = (char **) malloc(nrDrinks * sizeof(char *));
-        pricesDrinks = (double *) malloc(nrDrinks * sizeof(double));
-        gets(string);
-        strrev(string); // reversed the string so that if drink name contain '-' it won't affect the program
-        pt = strtok(string, "-");
-        for (int i = nrDrinks-1; i >= 0; i--) {
-            if (i != nrDrinks-1)pt = strtok(NULL, "-");
-            sscanf(strrev(pt), "%lf", &pricesDrinks[i]);
-            drinks[i] = (char *) malloc(MAX_DRINK_NAME * sizeof(char));
-            pt = strtok(NULL, "(");
-            strcpy(drinks[i], pt);
-            strrev(drinks[i]);
-        }
-    } else {
-        char string[MAX_STRING],line[MAX_LINE];
-        char *pt;
-        //loading food data
-        fgets(line,MAX_LINE,data);
-        line[strlen(line)-1] = '\0';
-        sscanf(line,"%d",&nrOfFoodTypes);
-        foodTypes = (char **) malloc(nrOfFoodTypes * sizeof(char *));
-        specFoods = (char ***) malloc(nrOfFoodTypes * sizeof(char **));
-        priceFoods = (double **) malloc(nrOfFoodTypes * sizeof(double *));
-        nrSpecType = (int *) malloc(nrOfFoodTypes * sizeof(int));
-        for (int i = 0; i < nrOfFoodTypes; i++) {
-            fgets(string,MAX_STRING,data);
-            pt = strtok(string, " ");
-            foodTypes[i] = (char *) malloc(MAX_FOOD_TYPE_NAME * sizeof(char));
-            strcpy(foodTypes[i], pt);
-            pt = strtok(NULL, ":");
-            sscanf(pt,"%d",&nrSpecType[i]);
-            specFoods[i] = (char **) malloc(nrSpecType[i] * sizeof(char *));
-            priceFoods[i] = (double *) malloc(nrSpecType[i] * sizeof(double));
-            for(int j=0;j<nrSpecType[i];j++) {
-                pt = strtok(NULL, "-");
-                specFoods[i][j] = (char *) malloc(MAX_SPEC_TYPE_NAME * sizeof(char));
-                strcpy(specFoods[i][j], pt);
-                specFoods[i][j][strlen(pt) - 1] = '\0';
-                strcpy(specFoods[i][j],specFoods[i][j]+2);
-                pt = strtok(NULL, ")");
-                sscanf(pt, "%lf", &priceFoods[i][j]);
-            }
-        }
-        //loading drinks data
-        fgets(line,MAX_LINE,data);
-        line[strlen(line)-1] = '\0';
-        sscanf(line,"%d",&nrDrinks);
-        drinks = (char **) malloc(nrDrinks * sizeof(char *));
-        pricesDrinks = (double *) malloc(nrDrinks * sizeof(double));
+    }
+    char string[MAX_STRING],line[MAX_LINE];
+    char *pt;
+    //loading food data
+    fgets(line,MAX_LINE,data);
+    //line[strlen(line)-1] = '\0';
+    sscanf(line,"%d",&nrOfFoodTypes);
+    foodTypes = (char **) malloc(nrOfFoodTypes * sizeof(char *));
+    specFoods = (char ***) malloc(nrOfFoodTypes * sizeof(char **));
+    priceFoods = (double **) malloc(nrOfFoodTypes * sizeof(double *));
+    nrSpecType = (int *) malloc(nrOfFoodTypes * sizeof(int));
+    for (int i = 0; i < nrOfFoodTypes; i++) {
         fgets(string,MAX_STRING,data);
-        string[strlen(string)-1] = '\0';
-        strrev(string); // reversed the string so that if drinks name contain '-' it won't affect the program
-        pt = strtok(string, "-");
-        for (int i = nrDrinks-1; i >= 0; i--) {
-            if (i != nrDrinks-1)pt = strtok(NULL, "-");
-            sscanf(strrev(pt), "%lf", &pricesDrinks[i]);
-            drinks[i] = (char *) malloc(MAX_DRINK_NAME * sizeof(char));
-            pt = strtok(NULL, "(");
-            strcpy(drinks[i], pt);
-            strrev(drinks[i]);
+        pt = strtok(string, " ");
+        foodTypes[i] = (char *) malloc(MAX_FOOD_TYPE_NAME * sizeof(char));
+        strcpy(foodTypes[i], pt);
+        pt = strtok(NULL, ":");
+        sscanf(pt,"%d",&nrSpecType[i]);
+        specFoods[i] = (char **) malloc(nrSpecType[i] * sizeof(char *));
+        priceFoods[i] = (double *) malloc(nrSpecType[i] * sizeof(double));
+        for(int j=0;j<nrSpecType[i];j++) {
+            pt = strtok(NULL, "-");
+            specFoods[i][j] = (char *) malloc(MAX_SPEC_TYPE_NAME * sizeof(char));
+            strcpy(specFoods[i][j], pt);
+            specFoods[i][j][strlen(pt) - 1] = '\0';
+            strcpy(specFoods[i][j],specFoods[i][j]+2);
+            pt = strtok(NULL, ")");
+            sscanf(pt, "%lf", &priceFoods[i][j]);
         }
+    }
+    //loading drinks data
+    fgets(line,MAX_LINE,data);
+    line[strlen(line)-1] = '\0';
+    sscanf(line,"%d",&nrDrinks);
+    drinks = (char **) malloc(nrDrinks * sizeof(char *));
+    pricesDrinks = (double *) malloc(nrDrinks * sizeof(double));
+    fgets(string,MAX_STRING,data);
+    string[strlen(string)-1] = '\0';
+    strrev(string); // reversed the string so that if drinks name contain '-' it won't affect the program
+    pt = strtok(string, "-");
+    for (int i = nrDrinks-1; i >= 0; i--) {
+        if (i != nrDrinks-1)pt = strtok(NULL, "-");
+        sscanf(strrev(pt), "%lf", &pricesDrinks[i]);
+        drinks[i] = (char *) malloc(MAX_DRINK_NAME * sizeof(char));
+        pt = strtok(NULL, "(");
+        strcpy(drinks[i], pt);
+        strrev(drinks[i]);
     }
     //load data for cutlery and addinfo
     char cutlery[][MAX_CUTLERY_NAME] = {"Yes","No,thanks"}, addInfo[200];
